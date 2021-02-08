01f980d3e91d193e23c6eee0b5362b4aad81fd36
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-08 10:07:51 +0100

    Ejercicio 1 - Proyecto sin Autentificacion

---
+++ EX1/EX1/Models/ex1Context.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace EX1.Models
+{
+    public partial class ex1Context : DbContext
+    {
+
+        public ex1Context(DbContextOptions<ex1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Piezas> Piezas { get; set; }
+        public virtual DbSet<Proveedores> Proveedores { get; set; }
+        public virtual DbSet<Suministra> Suministra { get; set; }
+        public virtual DbSet<UserInfo> UserInfo { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Piezas>(entity =>
+            {
+                entity.HasKey(e => e.Codigo)
+                    .HasName("pk_piezas");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Proveedores>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasMaxLength(4)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Suministra>(entity =>
+            {
+                entity.HasKey(e => new { e.CodigoPieza, e.IdProveedor })
+                    .HasName("pk_suministra");
+
+                entity.Property(e => e.IdProveedor)
+                    .HasMaxLength(4)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.HasOne(d => d.CodigoPiezaNavigation)
+                    .WithMany(p => p.Suministra)
+                    .HasForeignKey(d => d.CodigoPieza)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_suministra1");
+
+                entity.HasOne(d => d.IdProveedorNavigation)
+                    .WithMany(p => p.Suministra)
+                    .HasForeignKey(d => d.IdProveedor)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("fk_suministra2");
+            });
+
+            modelBuilder.Entity<UserInfo>(entity =>
+            {
+                entity.HasKey(e => e.UserId)
+                    .HasName("PK__UserInfo__1788CC4C1C8A7974");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
